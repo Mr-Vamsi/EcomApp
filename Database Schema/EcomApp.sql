e3e4bf0972a50a30aeef08832d000cf2d9b3920d
Mr-Vamsi <mrvamsi998922@gmail.com>
2024-07-09 03:07:39 +0530

    added Database schema

---
+++ Database Schema/EcomApp.sql
@@ -0,0 +1,19 @@
+create Database EcomDB;
+use EcomDB;
+CREATE TABLE usersData (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(45) NOT NULL,
+    useremail VARCHAR(45) NOT NULL,
+    usermobile VARCHAR(10) NOT NULL,
+    userpassword VARCHAR(255) NOT NULL);
+    select * from users_Data;    
+    create table products_Data(
+    sku int auto_increment primary key,
+    productname varchar(50) not null,
+    productdescription varchar(50) not null,
+    productquantity varchar(50) not null,
+    productcost varchar(50) not null
+    )
+    select * from products_Data;
+    drop table productsData;
+    
